5377f63575efbeaa4c4a9faba1642f1a892609fd
Olivier MATHE <omathe@free.fr>
2023-01-11 17:37:48 +0100

    record

--- java14/src/main/java/fr/ikisource/oma/java14/record/Range.java
+++ java14/src/main/java/fr/ikisource/oma/java14/record/Range.java
@@ -2,4 +2,8 @@ package fr.ikisource.oma.java14.record;
 
 public record Range(int begin, int end) {
 
+    public Range(int begin, int end) {
+        this.begin = begin;
+        this.end = end;
+    }
 }
