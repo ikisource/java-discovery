3bb9a494825c419c20d7b677a39ebdd047b5691b
Olivier MATHE <omathe@free.fr>
2023-01-23 16:08:03 +0100

    enrich text blocks feature

--- java15/src/main/java/fr/ikisource/oma/java15/textBlocks/TextBlocks.java
+++ java15/src/main/java/fr/ikisource/oma/java15/textBlocks/TextBlocks.java
@@ -1,15 +1,51 @@
 package fr.ikisource.oma.java15.textBlocks;
 
+import java.util.List;
+
 public class TextBlocks {
+
+    public static void printString() {
+
+        String s = """
+                "one"
+                'two'
+                three
+                four
+                five
+                """;
+        System.out.println(s);
+    }
+
+    public static void printIdentedString() {
+
+        // l'indentation est prise en compte à partir de la ligne verticale verte sous IntelliJ 🐸
+        String s = """
+                "one"
+                    'two'
+                three
+                    four
+                five
+                """;
+        System.out.println(s);
+    }
+
+    public static void printCharsAfter() {
+
+        String s = """
+                "one"        \s
+                    'two'    \s
+                three        \s
+                    four     \s
+                five         \s
+                """;
+        List<String> lines = s.lines().map(string -> "|" + string + "|").toList();
+        lines.forEach(System.out::println);
+    }
 
     public static void main(String[] args) {
 
-        String text = """
-                {
-                    "name" : "Foo", \
-                    "surname" : "Buu" \s
-                }
-                """;
-        System.out.println("text = " + text);
+        TextBlocks.printString();
+        TextBlocks.printIdentedString();
+        TextBlocks.printCharsAfter();
     }
 }
--- java19/README.md
+++ java19/README.md
@@ -2,3 +2,4 @@
 
 - JEP 405 : Record Patterns (Preview) 
 - JEP 425 : Virtual Threads (Preview)
+- JEP 427 :	Pattern Matching for switch (Third Preview)
